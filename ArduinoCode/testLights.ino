aa89ed84eda521d79854f520f532ce08b329036d
Z-Hilton <97143225+Z-Hilton@users.noreply.github.com>
2024-08-01 09:51:51 -0600

    Updated testLights.ino
    Updated the pins to work with the new NodeMCU esp32 and corresponding PCB. The new code has the lights always dimly on to help the cameras focus. The main flash is the same as before.

--- ArduinoCode/testLights.ino
+++ ArduinoCode/testLights.ino
@@ -1,62 +1,89 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
-#define LED_PIN 15      // Pin connected to the Data Input of WS2812B
+#define RGB_LED_PIN 15      // Pin connected to the Data Input of WS2812B
 #define NUM_LEDS 24     // Number of WS2812B LEDs in the strip
-#define BRIGHTNESS 200  // Set the brightness (0-255)
+#define LED_TYPE    WS2812B  // Adjust the LED type accordingly
+#define BRIGHTNESS 255  // Set the brightness (0-255)
 
-#define SHUTTER_PIN 14  //Pin connected to the shutter signal of the cameras via 3.5mm jack
-Adafruit_NeoPixel strip;
+#define SHUTTER_PIN 34  //Pin connected to the shutter signal of the cameras via 3.5mm jack
+
+CRGB leds[NUM_LEDS];
 
 void setup() {
   // setup code here, to run once:
-  pinMode(23, OUTPUT); // set each bank of 6 White Leds as outputs
-  pinMode(21, OUTPUT);
+  pinMode(18, OUTPUT); // set each bank of 6 White Leds as outputs
   pinMode(19, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(2, OUTPUT);                                                                                                                                                                                                                                                                                 
+  pinMode(21, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+  pinMode(25, OUTPUT);  
+  digitalWrite(18, LOW);
+  digitalWrite(19, LOW);
+  digitalWrite(21, LOW);
+  digitalWrite(22, LOW);
+  digitalWrite(23, LOW);
+  digitalWrite(25, LOW);                                                                                                                                                                                                                                                                               
   // set the shutterPin as a normally-high input
   pinMode(SHUTTER_PIN, INPUT_PULLUP); 
   // To control rgb leds
-  strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800); 
-  strip.begin();
-  strip.setBrightness(BRIGHTNESS);
-  strip.show();  // Initialize all pixels to off
+  FastLED.addLeds<LED_TYPE, RGB_LED_PIN, GRB>(leds, NUM_LEDS);
+  pinMode(SHUTTER_PIN, INPUT_PULLUP);
+  FastLED.clear();  // Turn off all LEDs initially
 
-// WARNING DO NOT USE ANALOG WRITE! YOU WILL BRICK THE ESP32.
-// I found out the hard way...
-  // analogWrite(23, 50); 
-  // analogWrite(21, 50);
-  // analogWrite(19, 50);
-  // analogWrite(5, 50);
-  // analogWrite(16, 50);
-  // analogWrite(2, 50);
+
+
 }
 
 void loop() {
   //main code here, to run repeatedly:
-  // if the shutter signal drops, flash on and off very fast (no delay)
-  if (digitalRead(SHUTTER_PIN) == 0) { 
-    digitalWrite(23, HIGH);
-    digitalWrite(21, HIGH);
+  // if the shutter signal drops, flash on and off
+  if (digitalRead(SHUTTER_PIN) != 0) { 
+    digitalWrite(18, HIGH);
     digitalWrite(19, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(16, HIGH);
-    digitalWrite(2, HIGH);
-    delay(10); 
-    digitalWrite(23, LOW);
-    digitalWrite(21, LOW);
+    digitalWrite(21, HIGH);
+    digitalWrite(22, HIGH);
+    digitalWrite(23, HIGH);
+    digitalWrite(25, HIGH); 
+    delayMicroseconds(10); 
+    digitalWrite(18, LOW);
     digitalWrite(19, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(16, LOW);
-    digitalWrite(2, LOW);
-    delay(500);
-    //Set the brightness of the LEDS to low (for cameras to focus)
-    // analogWrite(23, 50); 
-    // analogWrite(21, 50); //seems to cause the board to heat up...
-    // analogWrite(19, 50);
-    // analogWrite(5, 50);
-    // analogWrite(16, 50);
-    // analogWrite(2, 50);
+    digitalWrite(21, LOW);
+    digitalWrite(22, LOW);
+    digitalWrite(23, LOW);
+    digitalWrite(25, LOW); 
+    delay(10);
+  } else {
+    turnOnRed();
+    digitalWrite(18, HIGH);
+    digitalWrite(19, HIGH);
+    digitalWrite(21, HIGH);
+    digitalWrite(22, HIGH);
+    digitalWrite(23, HIGH);
+    digitalWrite(25, HIGH); 
+    delay(2); 
+
+    FastLED.clear();
+    FastLED.show();
+    digitalWrite(18, LOW);
+    digitalWrite(19, LOW);
+    digitalWrite(21, LOW);
+    digitalWrite(22, LOW);
+    digitalWrite(23, LOW);
+    digitalWrite(25, LOW); 
+    delay(10);    
   }
+
+}
+
+void turnOnWhite() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::White;
+  }
+  FastLED.show();
+}
+void turnOnRed() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Red;
+  }
+  FastLED.show();
 }
